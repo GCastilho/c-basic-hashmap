4e957737d6fc72773da2da044c483b238dddda26
GCastilho <castilho.gabriel14@gmail.com>
2023-03-17 12:05:42 -0300

    hash_char_array

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int hash_char_array(char key[]) {
+	int sum = 0;
+	while (key != NULL && *key != '\0') {
+		sum += *key;
+		key++;
+	}
+	return sum;
+}
+
 int main(int argc, char const *argv[]) {
-	printf("Hello, World!");
+	char str[] = "Hello, world!";
+	int hash = hash_char_array(str);
+
+	printf("Hello, World!\nHash is %i\n", hash);
 	return 0;
 }
