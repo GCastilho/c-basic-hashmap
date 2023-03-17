6502fbbfca66515a1e9dae5481ef68a3de771d81
GCastilho <castilho.gabriel14@gmail.com>
2023-03-17 20:31:56 -0300

    Set data in HashMap

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int hash_char_array(char key[]) {
 	int sum = 0;
@@ -9,10 +10,76 @@ int hash_char_array(char key[]) {
 	return sum;
 }
 
-int main(int argc, char const *argv[]) {
-	char str[] = "Hello, world!";
-	int hash = hash_char_array(str);
+typedef struct Item {
+	char *key;
+	char *value;
+} Item;
 
-	printf("Hello, World!\nHash is %i\n", hash);
+typedef struct Node {
+	struct Node *next;
+	Item *value;
+} Node;
+
+typedef struct HashMap {
+	size_t capacity;
+	Node* *items;
+} HashMap;
+
+HashMap new_hash_map(int capacity) {
+	HashMap hash_map;
+	hash_map.capacity = capacity;
+	hash_map.items = malloc(sizeof(Node*) * capacity);
+	return hash_map;
+}
+
+int set_hashmap(HashMap *hash_map, char key[], char value[]) {
+	Item *item = malloc(sizeof(Item));
+	item->key = key;
+	item->value = value;
+	Node *node = malloc(sizeof(Node));
+	node->value = item;
+
+	int idx = hash_char_array(key) % hash_map->capacity;
+	Node *stored_node = hash_map->items[idx];
+	if (stored_node == NULL) {
+		hash_map->items[idx] = node;
+	} else {
+		while (stored_node->next != NULL) {
+			stored_node = stored_node->next;
+		}
+		stored_node->next = node;
+	}
+
+	return idx;
+}
+
+int main(int argc, char const *argv[]) {
+	int capacity = 10;
+	HashMap hash_map = new_hash_map(capacity);
+
+	int idx = set_hashmap(&hash_map, "this-is-the-key", "this-is-the-value");
+	printf(
+		"Idx: %i | Key: %s | Value: %s\n",
+		idx,
+		hash_map.items[idx]->value->key,
+		hash_map.items[idx]->value->value
+	);
+
+	idx = set_hashmap(&hash_map, "this-is-key-the", "this-is-the-value");
+	printf(
+		"Idx: %i | Key: %s | Value: %s\n",
+		idx,
+		hash_map.items[idx]->next->value->key,
+		hash_map.items[idx]->next->value->value
+	);
+
+	idx = set_hashmap(&hash_map, "key-the-this-is", "this-value-is-the");
+	printf(
+		"Idx: %i | Key: %s | Value: %s\n",
+		idx,
+		hash_map.items[idx]->next->next->value->key,
+		hash_map.items[idx]->next->next->value->value
+	);
+
 	return 0;
 }
